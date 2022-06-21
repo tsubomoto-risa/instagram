6ddac144ead61c6aa8958a1cf0d2fff163da6caa
tsubomoto-risa <tsubomoto_risa@coreprice.com>
2022-06-21 14:52:21 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by 坪本 梨沙 on 2022/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
